0619d48eb001a81e877ada5fb3aa46ef38aef7c9
marcski55 <marc.skwarczynski@revature.net>
2022-05-29 18:25:14 -0400

    added axios practice

--- src/AxiosPokemon/index.jsx
+++ src/AxiosPokemon/index.jsx
@@ -1,9 +1,24 @@
+import axios from 'axios';
 import * as React from 'react';
+import '../App.css';
+import './index.css';
+
+// https://pokeapi.co/api/v2/pokemon?limit=10000
+// body.results[index].name
 
 export default function AxiosPokemon() {
+  const [pokemon, setPokemon] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      const list = await axios
+        .get('https://pokeapi.co/api/v2/pokemon?limit=10000')
+        .then((res) => res.data.results);
+      setPokemon(list.map((elem, index) => <li key={index}>{elem.name}</li>));
+    })();
+  }, []);
   return (
-    <main>
-      <p>New Page!</p>
+    <main className='App App-header Align-top Align-left'>
+      <ul>{pokemon}</ul>
     </main>
   );
 }
